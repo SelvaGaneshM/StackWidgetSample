0433e92a74326eb807afefc8dc928a2aa64c5943
SelvaGaneshM <selvaganesh.developer@gmail.com>
2016-10-12 14:43:33 +0530

    Initial commit

---
+++ app/src/main/java/com/stackwidgetapp/constant/Constants.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Title:Class hold common constants
+ * Author:Ramesh 
+ *******************************************************************************/
+
+package com.stackwidgetapp.constant;
+
+public class Constants {
+
+	/* Constant imagesurl and text data for this sample */
+	public static String images[] = {
+			"https://d38lgsgffpqnpg.cloudfront.net/dynamic/uploads/motupatluarasargalinarasan3d/app/600x400.jpg",
+			"https://d38lgsgffpqnpg.cloudfront.net/dynamic/uploads/fuddu/app/600x400.jpg",
+			"https://d38lgsgffpqnpg.cloudfront.net/dynamic/uploads/theaccountant/app/600x400.jpg"};
+
+	public static String text[] = { "Guardians of the Galaxy", "Sin City",
+			"Teenage Mutant Ninja Turtles"};
+
+	/* Preference Tags Used in this example */
+	public static String stackWidgettempId = "tempId";
+
+	public static String WidgetInfo = "WidgetInfo";
+
+	public static String testImageTag = "Image";
+
+	public static String testTextTag = "Text";
+
+}
